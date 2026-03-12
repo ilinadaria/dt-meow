d29af91e9a7c90667c19a8457d1a1052cbcc7285
dasha <daria.ilina@tum.de>
2026-03-12 22:23:17 +0300

    implemented logic for signs detection

--- packages/experimental/object_detection/include/object_detection/model.py
+++ packages/experimental/object_detection/include/object_detection/model.py
@@ -1,19 +1,21 @@
 import os
 from typing import Tuple
+
 import numpy as np
+
 import torch
 import subprocess
-process = subprocess.Popen(['pip', 'install', 'dt-data-api-daffy>=0.1.8'])
-process.wait()
-process = subprocess.Popen(['pip', 'install', 'ipython'])
-process.wait()
+from ultralytics import YOLO
+
 from dt_data_api import DataClient
+from solution.integration_activity import MODEL_NAME, DT_TOKEN
+
 from dt_device_utils import DeviceHardwareBrand, get_device_hardware_brand
-import rospy
+
+from .constants import IMAGE_SIZE, ASSETS_DIR
 
 JETSON_FP16 = True
-ASSETS_DIR = "/code/catkin_ws/src/object-detection/assets"
-IMAGE_SIZE = 416
+
 
 
 def run(input, exception_on_failure=False):
@@ -33,91 +35,55 @@ def run(input, exception_on_failure=False):
     return program_output.strip()
 
 
-class ModelWrapper():
-    def __init__(self, model_name: str, dt_token: str, aido_eval=False):
+class Wrapper:
+    def __init__(self, aido_eval=False):
+        model_name = MODEL_NAME()
 
         models_path = os.path.join(ASSETS_DIR, "nn_models")
-        dcss_models_path = "project/mooc/objdet/data/nn_models/"
-
-        dcss_weight_file_path = os.path.join(dcss_models_path, f"{model_name}.pt")
         weight_file_path = os.path.join(models_path, f"{model_name}.pt")
 
-        if aido_eval:
-            assert os.path.exists(weight_file_path)
-        else:
+        print(f"Looking for local model at: {weight_file_path}")
 
-            if get_device_hardware_brand() == DeviceHardwareBrand.JETSON_NANO:
-                # when running on the robot, we store models in the persistent `data` directory
-                models_path = "/data/nn_models"
-                weight_file_path = os.path.join(models_path, f"{model_name}.pt")
+        if not os.path.exists(weight_file_path):
+            raise FileNotFoundError(
+                f"Local model not found at: {weight_file_path}"
+            )
 
-            # make models destination dir if it does not exist
-            if not os.path.exists(models_path):
-                os.makedirs(models_path)
-
-            # open a pointer to the DCSS storage unit
-            client = DataClient(dt_token)
-            storage = client.storage("user")
-
-            # make sure the model exists
-            metadata = None
-            try:
-                metadata = storage.head(dcss_weight_file_path)
-            except FileNotFoundError:
-                print(f"FATAL: Model '{model_name}' not found. It was expected at '{dcss_weight_file_path}'.")
-                exit(1)
-
-            # extract current ETag
-            remote_etag = eval(metadata["ETag"])
-            print(f"Remote ETag for model '{model_name}': {remote_etag}")
-
-            # read local etag
-            local_etag = None
-            etag_file_path = f"{weight_file_path}.etag"
-            if os.path.exists(etag_file_path):
-                with open(etag_file_path, "rt") as fin:
-                    local_etag = fin.read().strip()
-                print(f"Found local ETag for model '{model_name}': {local_etag}")
-            else:
-                print(f"No local model found with name '{model_name}'")
-
-            # do not download if already up-to-date
-            print(f"DEBUG: Comparing [{local_etag}] <> [{remote_etag}]")
-            if local_etag != remote_etag:
-                if local_etag:
-                    print(f"Found a different model on DCSS.")
-                print(f"Downloading model '{model_name}' from DCSS...")
-                # download model
-                download = storage.download(dcss_weight_file_path, weight_file_path, force=True)
-                download.join()
-                assert os.path.exists(weight_file_path)
-                # write ETag to file
-                with open(etag_file_path, "wt") as fout:
-                    fout.write(remote_etag)
-                print(f"Model with ETag '{remote_etag}' downloaded!")
-            else:
-                print(f"Local model is up-to-date!")
-
-        # load pytorch model
         self.model = Model(weight_file_path)
 
-    def infer(self, image: np.ndarray) -> Tuple[list, list, list]:
+        print("Loaded YOLO classes:")
+        for i, name in self.model.model.names.items():
+            print(i, ":", name)
+
+    def predict(self, image: np.ndarray) -> Tuple[list, list, list]:
         return self.model.infer(image)
-
-import subprocess
-
 
 class Model:
     def __init__(self, weight_file_path: str):
+        print("HELLO")
         super().__init__()
 
-        model = torch.hub.load("ultralytics/yolov5", "custom", path=weight_file_path)
+        print("HIIII")
+
+        if not os.path.exists("/yolov5/hubconf.py"):
+            print("Cloning YOLOv5...")
+            subprocess.check_call("git clone -b v6.2 https://github.com/ultralytics/yolov5.git /yolov5", shell=True)
+
+        model = torch.hub.load("/yolov5", "custom", path=weight_file_path, source="local")
+        #model = YOLO(weight_file_path)
         model.eval()
 
-        use_fp16: bool = JETSON_FP16 and get_device_hardware_brand() == DeviceHardwareBrand.JETSON_NANO
+        print(model)
+
+
+        use_fp16 = (JETSON_FP16 and torch.cuda.is_available() and get_device_hardware_brand() == DeviceHardwareBrand.JETSON_NANO)
 
         if use_fp16:
+            print("Using FP16")
             model = model.half()
+        else:
+            print("Using FP32")
+
 
         if torch.cuda.is_available():
             self.model = model.cuda()
@@ -127,6 +93,7 @@ class Model:
         del model
 
     def infer(self, image: np.ndarray) -> Tuple[list, list, list]:
+        print("HIII")
         det = self.model(image, size=IMAGE_SIZE)
 
         xyxy = det.xyxy[0]  # grabs det of first image (aka the only image we sent to the net)
--- packages/object_detection/src/object_detection_node.py
+++ packages/object_detection/src/object_detection_node.py
@@ -7,10 +7,18 @@ from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import Twist2DStamped
 from cv_bridge import CvBridge
 from sensor_msgs.msg import CompressedImage
-from std_msgs.msg import Int16  # Imports msg
+from std_msgs.msg import Int16 
 from nn_model.constants import IMAGE_SIZE
 from nn_model.model import Wrapper
 from solution.integration_activity import NUMBER_FRAMES_SKIPPED
+from duckietown_msgs.msg import (
+    AprilTagsWithInfos,
+    TagInfo,
+    BoolStamped,
+    AprilTagDetection,
+    AprilTagDetectionArray,
+)
+from geometry_msgs.msg import Transform, Vector3, Quaternion
 
 class ObjectDetectionNode(DTROS):
     def __init__(self, node_name):
@@ -23,6 +31,7 @@ class ObjectDetectionNode(DTROS):
         self.pub_vel = rospy.Publisher(f"/{self.veh}/car_cmd_switch_node/cmd", Twist2DStamped, queue_size=1)
         self.pub_detections_image = rospy.Publisher("~image/compressed", CompressedImage, queue_size=1)
         self.pub_detection = rospy.Publisher("~sign", Int16, queue_size = 1)
+        self.publish_fake_apriltag_detections("~detections", AprilTagDetectionArray, queue_size=1)
         
         
         self.sub_image = rospy.Subscriber(
@@ -39,6 +48,15 @@ class ObjectDetectionNode(DTROS):
         
         self.initialized = True
         self.log("Initialized!")
+
+        # this is cursed
+        self.class_to_tag_id = {
+            0: 8,   # 4-way-intersect
+            1: 11,  # T-intersection
+            3: 4,   # no-left-turn
+            4: 3,   # no-right-turn
+            5: 1    # stop
+        }
 
 # this callback should be changed to a different logic (for traffic signs)
     def image_cb(self, image_msg):
@@ -59,7 +77,8 @@ class ObjectDetectionNode(DTROS):
         rgb = bgr[..., ::-1] # reverse from bgr to rgb
         rgb = cv2.resize(rgb, (IMAGE_SIZE, IMAGE_SIZE))
 
-        bboxes, classes, scores = self.model_wrapper.predict(rgb)
+        # bboxes, classes, scores = self.model_wrapper.predict(rgb)
+        bboxes, classes, scores = self.model_wrapper.predict_and_filter(rgb)
         for cls, bbox, score in zip(classes, bboxes, scores):
             print(f"cls={cls}, bbox={bbox}, score={score}")
 
@@ -78,20 +97,15 @@ class ObjectDetectionNode(DTROS):
             # Calculate center of bounding box
             center_x = (bbox[0] + bbox[2]) / 2
 
-            if cls == 5 and score > 0.5:  # Duck
+            if cls == 2 and score > 0.5:  # Duck
                 area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
                 print("AREA : " + str(area))
                 if area > 500 and left_boundary < center_x < right_boundary:
                     stop_signal = True
                     large_duck = True
                     self.log(f"Stop sign")
-
-            if cls == 1 and score > 0.7:  # Duckiebot
-                area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
-                if area > 10000 and left_boundary < center_x < right_boundary:
-                    stop_signal = True
-                    large_duckiebot = True
-                    self.log(f"Duckiebot detected. Area: {area}")
+            else:
+                self.publish_fake_apriltag_detections(image_msg, bboxes, classes, scores)
 
         # Create velocity command
         vel_cmd = Twist2DStamped()
@@ -106,12 +120,8 @@ class ObjectDetectionNode(DTROS):
                 self.log("Stopping for Duckiebot.")
             else:
                 self.log("Stopping for duck and duckiebot.")
-        else:
-            vel_cmd.v = 0
-            vel_cmd.omega = 0.0
-            self.log("Driving...")
-        
-        self.pub_vel.publish(vel_cmd)
+            
+            self.pub_vel.publish(vel_cmd)
 
 
         self.visualize_detections(rgb, bboxes, classes, scores)
@@ -133,6 +143,82 @@ class ObjectDetectionNode(DTROS):
         bgr = rgb[..., ::-1]
         obj_det_img = self.bridge.cv2_to_compressed_imgmsg(bgr)
         self.pub_detections_image.publish(obj_det_img)
+
+    def bbox_to_fake_apriltag_pose(self, bbox):
+        # VERY rough transation from bbox to camera pose, since we only use it for random_apriltags node
+
+        x1, y1, x2, y2 = bbox
+
+        w = max(float(x2 - x1), 1.0) # avoid division by 0
+        h = max(float(y2 - y1), 1.0)
+        area = w * h
+
+        # bbox center
+        cx = (x1 + x2) / 2.0
+        cy = (y1 + y2) / 2.0
+
+        # normalized image offsets from center
+        # these are dimensionless and roughly in [-0.5, 0.5]
+        x_offset = (cx - IMAGE_SIZE / 2.0) / IMAGE_SIZE
+        y_offset = (cy - IMAGE_SIZE / 2.0) / IMAGE_SIZE
+
+        # crude depth proxy:
+        # bigger bbox area => smaller z => closer sign
+        z = 5000.0 / area
+
+        # convert image offset into rough camera-frame x/y
+        x = x_offset * z
+        y = y_offset * z
+
+        return x, y, z
+        
+    def publish_fake_apriltag_detections(self, image_msg, bboxes, classes, scores):
+        det_array = AprilTagDetectionArray()
+        det_array.header.stamp = image_msg.header.stamp
+        det_array.header.frame_id = image_msg.header.frame_id
+
+        for cls, bbox, score in zip(classes, bboxes, scores):
+            cls = int(cls)
+
+            if cls not in self.class_to_tag_id:
+                continue
+
+            if score < 0.5:
+                continue
+
+            x, y, z = self.bbox_to_fake_apriltag_pose(bbox)
+
+            cx = (bbox[0] + bbox[2]) / 2
+            cy = (bbox[1] + bbox[3]) / 2
+
+            corners = [
+                    bbox[0], bbox[1],
+                    bbox[2], bbox[1],
+                    bbox[2], bbox[3],
+                    bbox[0], bbox[3]
+                ]
+            
+            tag_id = self.class_to_tag_id[cls]
+
+            detection = AprilTagDetection(
+                transform=Transform(
+                    translation=Vector3(x=x, y=y, z=z),
+                    rotation=Quaternion(x=0.0, y=0.0, z=0.0, w=1.0), #assume no rotation
+                ),
+                tag_id=tag_id,
+                tag_family="tag36h11",
+                hamming=0,
+                decision_margin=float(score)*100,
+                homography=[0.0]*9,
+                center=[cx, cy],
+                corners=corners,
+                pose_error=0.0
+            )
+
+
+            det_array.detections.append(detection)
+
+        self.pub_detections.publish(det_array)
 
 if __name__ == "__main__":
     object_detection_node = ObjectDetectionNode(node_name="object_detection_node")
